59e57e662762e711363c92061396838caaf10c11
Kevin Rocker <kevin.j.rocker@gmail.com>
2019-12-29 21:11:15 -0500

    Dedicated Histogram Endpoint

--- src/api/apiService.ts
+++ src/api/apiService.ts
@@ -37,5 +37,24 @@ export const createApiService = (axios: AxiosInstance) => ({
     return axios.get("/pages/select", { params }) as Promise<
       ApiResponse<SelectData[], any>
     >;
+  },
+  getRevisionsHistogram: (params: {
+    language: string;
+    field: "createdAt" | "textLength";
+  }) => {
+    return axios.get("/histogram/revisions", { params }) as Promise<
+      ApiResponse<
+        { floor: any; ceiling: any; count: number },
+        { minimum: any; maximum: any }
+      >
+    >;
+  },
+  getPagesHistogram: (params: { language: string; field: "revisionCount" }) => {
+    return axios.get("/histogram/pages", { params }) as Promise<
+      ApiResponse<
+        { floor: any; ceiling: any; count: number },
+        { minimum: any; maximum: any }
+      >
+    >;
   }
 });
